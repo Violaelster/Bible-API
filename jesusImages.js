d83d6e45c7755f3f8f1c2f1a33aacd24f880f537
Violaelster <viola.svea@gmail.com>
2024-12-06 13:33:43 +0100

    Removed option for changing theme colours

---
+++ jesusImages.js
@@ -0,0 +1,17 @@
+const button = document.getElementById('imageButton'); //Hämtar knapp som byter bild
+const image = document.getElementById('image'); //Hämtar bild-element
+
+button.addEventListener('click',fetchAndChangeImage); //Startar funktionen för att byta bild när knappen klickas
+
+function fetchAndChangeImage() {
+    fetch('https://pixabay.com/api/?key=47486558-491f2e0bd96e0464a2508cdfd&q=jesus&image_type=photo')
+    .then(response => response.json())
+    .then(data => {
+        const randomIndex = Math.floor(Math.random() * data.hits.length);
+        image.src = data.hits[randomIndex].webformatURL;
+    });
+}
+
+console.log(document.getElementById('imageButton')); // Bör skriva ut elementet eller `null`
+console.log(document.getElementById('image')); // Bör skriva ut elementet eller `null`
+
